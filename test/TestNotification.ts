435e38cc224749fe2deb7d42fc4420581b3a0203
Chris Gelon <chrisgelon@outlook.com>
2017-08-24 01:34:42 -0700

    Add Tests to master-notifications Implement mocha to allow for tests to be created for master-notifications. Added a small constructor test for Notification.ts

---
+++ test/TestNotification.ts
@@ -0,0 +1,20 @@
+import { suite, test, slow, timeout } from "mocha-typescript";
+import { expect } from "chai";
+import Notification from "../src/ts/Notification";
+
+@suite(timeout(3000), slow(1000))
+class TestNotification {
+    @test("Test standard Notification constructor") public TestConstructor(): void {
+        const message: string = "this is a message";
+        const notification: Notification = new Notification(message, "success", {});
+        expect(notification.message).to.equal(message);
+    }
+    
+    /**
+     * A method to allow for JavaScript threads to sleep using await.
+     * @param timeInMilliseconds The amount of time to sleep, in milliseconds.
+     */
+    private sleep(timeInMilliseconds: number): Promise<void> {
+        return new Promise<void>(resolve => setTimeout(resolve, timeInMilliseconds));
+    }
+}
